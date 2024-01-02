8b543bcf613bca81beeb83ea9e1ebe0a9dde7808
Mahir1902 <mahirhaqe7@gmail.com>
2024-01-02 15:46:49 +1100

    set up home page and styles. Implemented login button

---
+++ components/auth/LoginButton.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React from 'react'
+import { useRouter } from 'next/navigation'
+
+type Props = {
+    children: React.ReactNode
+    mode?: 'modal' | 'redirect'
+    asChild?: boolean
+}
+
+export default function LoginButton({children, mode='redirect', asChild}: Props) {
+
+
+    const router = useRouter()
+
+    const onClick = () => {
+        router.push('auth/login')
+    }
+
+    if(mode === 'modal') {
+        return (
+            <span>TODO: Implement modal</span>
+        )
+    }
+
+    return (
+
+    <span className='cursor-pointer' onClick={onClick}>
+        {children}
+    </span>
+  )
+}
